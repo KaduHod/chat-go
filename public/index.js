1ede431da01ba7a52702407ad6b85fd2e5af1f64
kaduhod <carlosjr.ribas@gmail.com.br>
2024-07-06 12:47:17 -0300

    :hammer: Adicionado cadastro ao front end :hammer:

--- public/index.js
+++ public/index.js
@@ -1,52 +1,84 @@
-const ws = new WebSocket("wss://37a1-2804-14c-87c4-82fb-8e69-ca42-e2b0-69fa.ngrok-free.app/ws")
+const AMBIENTE = "DEV"//"PROD"
+const BASEURLSERVER = AMBIENTE == "DEV" ? "localhost:3000" : "37a1-2804-14c-87c4-82fb-8e69-ca42-e2b0-69fa.ngrok-free.app"
+const WEBSOCKETURL = BASEURLSERVER + "/ws"
+
 const div_resultado = document.getElementById("chat")
 const text = document.getElementById('input')
-const button = document.getElementById('botao')
+const botao = document.getElementById('botao')
 const botaocadastrar = document.getElementById('botaocadastrar')
 const inputnome = document.getElementById('nome');
 const inputapelido = document.getElementById('apelido');
+let usuario = null
+console.log({botaocadastrar})
+
 botaocadastrar.onclick = async (evt) => {
-	const avisaErro = () => alert('Nome e apelido devem ter no minimo 8 caracteres')
+	const avisaErro = (id = 1) => alert(`[${id}] Nome e apelido devem ter no minimo 8 caracteres`)
 	const nome = inputnome.value.trim()
 	const apelido = inputapelido.value.trim();
-	console.log({
-		len: nome.length,
-		len2: apelido.length
-	})
 	if(nome == "" || apelido == ""){
 		return avisaErro()
 	}
 	if(nome.length < 8 || apelido.length != 8) {
-		return avisaErro()
+		return avisaErro(2)
 	}
-	fetch
+	if(!(user = await logarRequest({nome, apelido}))) {
+		console.log({user})
+		return alert("Erro ao logar :(")	
+	}
+	esconderElemento("cadastro")
 }
 
+const esconderElemento = id => {
+	const elemento = document.getElementById(id)
+	if(elemento.classList.contains('escondido')) return
+	return elemento.classList.add('escondido')
+}
 
-button.onclick = () => {
-	ws.send(text.value)
-	text.value = ""
-}
-ws.onclose = (evt) => {
-	console.log("Conexão fechada")
-	escreveMensagem("Conexao fechada com o servidor de chat");
-}
-document.addEventListener("keydown", e => {
-	if (e.key === "Enter") {
-		button.click()
+const logarRequest = async (usuario) => {
+	const res = await fetch("http://" + BASEURLSERVER + "/logar", {
+		headers: {'Content-type':'application/json'},
+		method: "POST",
+		body: JSON.stringify(usuario)
+	})
+	const resposta = await res.json()
+	console.log({res, resposta})
+	usuario = resposta.usuario
+	if(res.status !== 200){
+		return false
 	}
-})
-ws.onmessage = (evt) => {
-	console.log({evt});
-	console.log(evt.data)
-	escreveMensagem("[anônimo]  " + evt.data)
+	return resposta.usuario
+}
+
+const conectarWS = async () => {
+	const ws = new WebSocket(WEBSOCKETURL)
+	botao.onclick = () => {
+		ws.send(text.value)
+		text.value = ""
+	}
+
+	ws.onclose = (evt) => {
+		console.log("Conexão fechada")
+		escreveMensagem("Conexao fechada com o servidor de chat");
+	}
+
+	document.addEventListener("keydown", e => {
+		if (e.key === "Enter") {
+			botao.click()
+		}
+	})
+
+	ws.onmessage = (evt) => {
+		console.log({evt});
+		console.log(evt.data)
+		escreveMensagem("[anônimo]  " + evt.data)
+	}
 }
 
 const escreveMensagem = msg => {
-	div_resultado.innerHTML += `<br> [${formatDate()}] ${msg}`
+	div_resultado.innerHTML += `<br> [${formatarData()}] ${msg}`
 }
 
-function formatDate() {
+function formatarData() {
     const now = new Date();
     const day = String(now.getDate()).padStart(2, '0');
     const month = String(now.getMonth() + 1).padStart(2, '0'); // Janeiro é 0!
