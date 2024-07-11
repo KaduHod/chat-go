5f8ec9b580ff3d2a0b7087a810dde1ff4aadb75f
kaduhod <carlosjr.ribas@gmail.com.br>
2024-07-11 08:26:36 -0300

    :cookie: Adicionado valor de aporte em grafico

--- public/scripts/grafico.js
+++ public/scripts/grafico.js
@@ -9,8 +9,17 @@ const resultadoComJuros = document.getElementById('resultado_com_juros');
 const resultadoSemJuros = document.getElementById('resultado_sem_juros');
 const resultadoDiferenca = document.getElementById('diferenca_juros');
 const maiorValorizacao = document.getElementById('maior_valorizacao');
+const maiorValorAportado = document.getElementById('maior_valor_aportado')
+const paraValorMonetario = v => v.toLocaleString('pt-BR', {currency:"BRL", style:'currency'});
+const init = async () => {
+	let url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eth.json"
+	const res = await fetch(url);
+	const {eth} = await res.json()
+	document.getElementById('precoeth').innerText = paraValorMonetario(eth.brl)
+}
+init();
 const taxa = document.getElementById('taxa')
-const AMBIENTE = "PROD"
+const AMBIENTE = "DEV"
 const URLBASE = AMBIENTE == "DEV" ? "http://localhost:3000" : "https://132f-2804-14c-87c4-82fb-83a5-9e0c-560d-fee5.ngrok-free.app" ;
 const chart = new Chart(document.getElementById('meu-grafico'), {
 	type: 'line', // Tipo de gráfico: 'line' para gráfico de linha
@@ -40,26 +49,34 @@ botaoSimular.onclick = async () => {
 	const valoresComJurosComposto = resultadoDePeriodos.map((d) => d.valorComAporteMaisJurosComposto);
 	const valoresSemJurosComposto = resultadoDePeriodos.map((d) => d.valorComAporteSemJurosComposto)
 	const valoresValorizacao = resultadoDePeriodos.map((d) => d.valorizacaoPeriodo)
+	const valorAportado = resultadoDePeriodos.map((d) => d.valorAportado)
 	const datas = resultadoDePeriodos.map((d) => d.data)
 	const dataSets = [
 		{
 			label: 'Rendimentos com juros composto',
 			data: valoresComJurosComposto, // Dados do eixo Y
-			borderColor: 'rgba(11, 13, 82, 1)', // Cor da linha
+			borderColor: 'rgba(245, 68, 55, 1)', // Cor da linha
 			tension: 0.1, // Tensão da linha (0 para linhas retas, 0.1 para suavizar)
 			fill: false // Não preencher área abaixo da linha
 		},
 		{
 			label: 'Rendimentos sem juros composto',
 			data: valoresSemJurosComposto, // Dados do eixo Y
-			borderColor: 'rgba(71, 11, 11, 1)', // Cor da linha
+			borderColor: 'rgba(114, 208, 245, 1)', // Cor da linha
 			tension: 0.1, // Tensão da linha (0 para linhas retas, 0.1 para suavizar)
 			fill: false // Não preencher área abaixo da linha
 		},
 		{
 			label: 'Valorização do retorno da taxa de juros',
 			data: valoresValorizacao, // Dados do eixo Y
-			borderColor: 'rgba(10, 80, 10, 23)', // Cor da linha
+			borderColor: 'rgba(68, 245, 124, 23)', // Cor da linha
+			tension: 0.1, // Tensão da linha (0 para linhas retas, 0.1 para suavizar)
+			fill: false // Não preencher área abaixo da linha
+		},
+		{
+			label: 'Valor aportado',
+			data: valorAportado, // Dados do eixo Y
+			borderColor: 'rgba(127, 136, 124, 23)', // Cor da linha
 			tension: 0.1, // Tensão da linha (0 para linhas retas, 0.1 para suavizar)
 			fill: false // Não preencher área abaixo da linha
 		},
@@ -71,5 +88,6 @@ botaoSimular.onclick = async () => {
 	resultadoComJuros.innerText = ultimoRegistroDePeriodo.valorComJurosCompostoFormatado;
 	resultadoSemJuros.innerText = ultimoRegistroDePeriodo.valorSemJurosCompostoFormatado;
 	maiorValorizacao.innerText = ultimoRegistroDePeriodo.valorizacaoPeriodoFormatado;
-	resultadoDiferenca.innerText = (ultimoRegistroDePeriodo.valorComAporteMaisJurosComposto - ultimoRegistroDePeriodo.valorComAporteSemJurosComposto).toLocaleString('pt-BR', {currency:"BRL", style:'currency'});
+	maiorValorAportado.innerText = paraValorMonetario(ultimoRegistroDePeriodo.valorAportado)
+	resultadoDiferenca.innerText = paraValorMonetario(ultimoRegistroDePeriodo.valorComAporteMaisJurosComposto - ultimoRegistroDePeriodo.valorComAporteSemJurosComposto);
 }
