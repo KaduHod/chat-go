0ee3114a3b6508e2d7e1c2cac9b27e667ade9116
kaduhod <carlosjr.ribas@gmail.com.br>
2024-07-27 21:50:03 -0300

    :hammer: removendo matrix desta branch

--- source/services/conexao_test.go
+++ source/services/conexao_test.go
@@ -1,19 +1,2 @@
 package services
 
-import (
-	"fmt"
-	"testing"
-)
-
-func TestMatrix(t *testing.T) {
-    cliente := NewClienteMatrix("kaduhod1", PROVEDOR_PADRAO, "cX2@hDEADv5kbpE")
-    cliente.SetIdDeUsuario()
-    if err := cliente.Login(); err != nil {
-        t.Fail()
-    }
-    if err := cliente.BuscarSalas(); err != nil {
-        t.Fail()
-    }
-    fmt.Println(cliente.Salas)
-    t.Fail()
-}
